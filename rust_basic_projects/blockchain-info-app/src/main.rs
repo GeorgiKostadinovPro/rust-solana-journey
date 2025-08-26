2de8d1143bd9e2e5747d5c03052baee58a07b687
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2025-08-26 21:42:51 +0300

    Finish requests for node status and address

--- rust_basic_projects/blockchain-info-app/src/main.rs
+++ rust_basic_projects/blockchain-info-app/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+// import dependencies
+use tokio;
+
+// use all macros from serde for serialization and deserialization
+#[macro_use]
+extern crate serde
+
+// Register modules in the crate
+mod node_api;
+mod node_status;
+mod node_address;
+mod node_transaction;
+
+// import modules 
+// get from current crate
+use crate::node_status::NodeStatus;
+use crate::node_address::NodeAddress;
+use crate::node_transaction::NodeTransaction;
+
+async fn node_info_app(address: &str) {
+    let node_status: NodeStatus = node_api::get_node_status().await;
+    print!("\n\nQuerying: {} from chain: {}\n\n", &node_status.blockbook.coin, &node_status.backend.chain);
+
+    let node_address: NodeAddress = node_api::get_node_address(&address).await;
+    print!("\n\nAnalyzing tx for Bitcoin address {}\n\n", &node_address.address);
+}
+
+#[tokio:main]
+async fn main() {
+    node_info_app("").await;
 }
--- rust_basic_projects/blockchain-info-app/src/node_api.rs
+++ rust_basic_projects/blockchain-info-app/src/node_api.rs
@@ -1,24 +1,27 @@
 // import dependencies
 use dotenv;
 use reqwest;
-use tokio;
 use serde_json::Result;
 
-// import modules // get from current crate (added in main.rs)
+// import modules
+// get from current crate (added in main.rs)
 use crate::node_status::NodeStatus;
 use crate::node_address::NodeAddress;
 use crate::node_transaction::NodeTransaction;
 
+// Constants
 const HOST_BASE: &str = "https://btcbook.nownodes.io/api/";
+
+// Errors
 const API_KEY_NOT_FOUND: &str = "API_KEY NOT FOUND";
 const RESPONSE_NOT_SUCCESSFUL: &str = "RESPONSE NOT SUCCESSFUL";
 const CONVERT_NOT_SUCCESSFUL: &str = "CONVERT NOT SUCCESSFUL";
+const SERIALIZATION_NOT_SUCCESSFUL: &str = "SERIALIZATION NOT SUCCESSFUL";
 
 /// @title request
 /// @author GeorgiKostadinovPro
-/// @notice async request sender
-/// @dev custom request sender using reqwest HTTP client
-#[tokio:main]
+/// @notice request sender
+/// @dev custom async request sender using reqwest HTTP client
 pub async fn request(url: &str) -> String {
     // create a client object (preconfigured with components)
     let client = reqwest::Client::new();
@@ -38,6 +41,21 @@ pub async fn request(url: &str) -> String {
         .expect(CONVERT_NOT_SUCCESSFUL)
 }
 
-pub async fn node_status_request() -> NodeStatus {
+/// @title get_node_status
+/// @author GeorgiKostadinovPro
+/// @notice get request fn for node status
+/// @dev custom async get request fn for node status
+pub async fn get_node_status() -> NodeStatus {
+    let res = request(HOST_BASE).await;
+    serde_json::from_str(&res).expect(SERIALIZATION_NOT_SUCCESSFUL)
+}
 
+/// @title get_node_address
+/// @author GeorgiKostadinovPro
+/// @notice get request fn for node address
+/// @dev custom async get request fn for node address
+pub async fn get_node_address(address: &str) -> NodeAddress {
+    let req = [HOST_BASE, "v2/address/", &address].join("");
+    let res = request(req).await;
+    serde_json::from_str(&res).expect(SERIALIZATION_NOT_SUCCESSFUL)
 }
