6279a2c7aec15213e35870bce530f1df7f99d3de
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2025-09-11 21:45:00 +0300

    Impl Walllet Module

---
+++ rust_projects/mining-pool-rest-api/src/models/wallet.rs
@@ -0,0 +1,29 @@
+use {
+    serde::{Serialize, Deserialize}
+};
+
+// JSON Payload (REST)
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Wallet {
+    pub address: String,
+    pub club_name: String,
+    // MH/s
+    pub total_hash_rate: i32,
+    pub total_mined_shares: i32,
+    pub total_online_workers: i32,
+    // wallet has many miners
+    pub online_workers: Vec<Miner>
+}
+
+// POST Request Body - Create Miner
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CreateWallet {
+    pub club_name: String
+}
+
+// Miner DAO (DB Table Records)
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MinerDAO {
+    pub address: String,
+    pub club_name: String
+}
